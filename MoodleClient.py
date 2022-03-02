6ed002b5b70cdce41b0d31367fe983963018f06a
eulalia505 <100749548+eulalia505@users.noreply.github.com>
2022-03-02 03:39:39 -0500

    Update MoodleClient.py

--- MoodleClient.py
+++ MoodleClient.py
@@ -11,7 +11,7 @@ class MoodleClient(object):
         self.username = user
         self.password = passw
         self.session = requests.Session()
-        self.path = 'https://eduvirtual.uho.edu.cu/'
+        self.path = 'https://cursos.uo.edu.cu/'
         self.userdata = ''
 
     def getsession(self):
